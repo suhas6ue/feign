126895fb0f3e7f2afc7e5e0379f6ffe530187173
Suhas Meena <suhas6ue@gmail.com>
2025-10-26 15:51:29 +0530

    hi this is formatted

--- src/main/java/com/example/feign/feign/FeignApplication.java
+++ src/main/java/com/example/feign/feign/FeignApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FeignApplication {
 
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(FeignApplication.class, args);
 	}
