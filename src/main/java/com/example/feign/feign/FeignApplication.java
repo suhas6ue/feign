eb41ee9901197f5e08cd682f89bbb979d944de0f
Suhas Meena <suhas6ue@gmail.com>
2025-10-26 15:48:59 +0530

    committed

--- src/main/java/com/example/feign/feign/FeignApplication.java
+++ src/main/java/com/example/feign/feign/FeignApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FeignApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(FeignApplication.class, args);
 	}
